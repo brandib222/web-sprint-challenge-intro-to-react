8a0753b18b0754cf7578251cd442533389857f11
Greg Ball <gregball@Gregs-MBP.attlocal.net>
2021-12-10 16:10:00 -0500

    just have the questions left

--- src/components/Character.js
+++ src/components/Character.js
@@ -6,11 +6,6 @@ import '../App.css';
 
 const StyledCharacter = styled.div
 `
-div {
-    border: 5px yellow;
-
-}
-
 button {
     font-size: 50px;
     width:500px;
@@ -20,7 +15,7 @@ button {
     background-image: url('https://images.unsplash.com/photo-1530508777238-14544088c3ed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8b3V0ZXIlMjBzcGFjZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60');
     background-size: cover;
     display:flex;
-    margin: 3%;
+    margin: 3% 0%;
     justify-content: center;
     align-items: center;
     box-shadow: 0 0 30px;
