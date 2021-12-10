d5a58743b7775a9960c94b967d84fef45b41f949
Greg Ball <gregball@Gregs-MBP.attlocal.net>
2021-12-10 13:25:51 -0500

    api is rendering properly

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
+import axios from 'axios';
+
 import './App.css';
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+
+  const [characters, setCharacters] = useState([])
+ /* const [characterIdx, setCharacterIdx] = useState(0);
+
+  const openCharacter = idx => {
+      setCharacterIdx(idx)
+    }
+  
+  const closeCharacter = () => {
+    setCharacterIdx(null)
+  }*/
+
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/people/`)
+      .then(res => {
+        setCharacters(res.data);
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  }, []);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -12,6 +36,11 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {
+        characters.map(ch => {
+          return <Character key={ch.mass} name={ch.name} />
+        })
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,10 @@
 // Write your Character component here
+import React from 'react';
+
+export default function Character(props) {
+    return (
+        <div className='character-container'>
+            <button className='name'>{props.name}</button>
+        </div>
+    )
+}
